1cfb39ce1d7eafaa7553679abed5a746aeca7adf
Arnulfo Sánchez <arnulfo457@gmail.com>
2023-08-10 15:11:59 -0400

    Create Notification 20:00 every day

--- Trivia Battle Game/Trivia Battle Game/Controllers/NotificationHelper.swift
+++ Trivia Battle Game/Trivia Battle Game/Controllers/NotificationHelper.swift
@@ -19,10 +19,50 @@ class NotificationHelper : ObservableObject {
                 print("ERROR : \(error)")
             } else{
                 print("SUCCESS")
+                NotificationHelper.instance.playAgainNotification()
             }
         }
     }
     
+    func playAgainNotification(){
+        print("send notification play again")
+        let content = UNMutableNotificationContent()
+        content.title = "Trivia Battle Game"
+        content.subtitle = "Come and play with us again!"
+        content.sound = .default
+        content.badge = 1
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 20
+        dateComponents.minute = 00
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+    }
     
+    func addEnergyNotification(){
+        print("send notification add energy")
+        let content = UNMutableNotificationContent()
+        content.title = "Trivia Battle Game"
+        content.subtitle = "We just added Energy!"
+        content.sound = .default
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+        
+    }
 
 }
